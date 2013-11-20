ad61419cfddfd7d092281a12844fc1f8766a3f27
Akkarinage <mike.langford@live.co.uk>
2013-11-20 16:16:33 +0000

    Removed buggy sql files

--- data/schemas/logindb/cp_cmspages.20131120145801.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `cp_cmspages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` varchar(100) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `body` text NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM 
--- data/schemas/logindb/cp_cmspages.20131120155001.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `cp_cmspages` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `path` varchar(100) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `body` text NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM 
-INSERT INTO `cp_cmspages` (`id` ,`path` ,`title` ,`body` ,`modified`) VALUES (NULL , 'rules', 'Rules', 'This is a rules page.', '2013-11-20 00:00:00');

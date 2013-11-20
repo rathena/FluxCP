d14ddc8810feeba82b24161f7a5a30e0b65b46f3
Akkarinage <mike.langford@live.co.uk>
2013-11-20 23:12:40 +0000

    Added initial support for FluxAdmin.
    Signed-off-by: Akkarinage <mike.langford@live.co.uk>

---
+++ themes/default/fluxadmin/index.php
@@ -0,0 +1,62 @@
+<?php
+/* FluxAdmin
+ * Created and maintained by Akkarin
+ * Current Version: 1.00.01
+ */
+ 
+if (!defined('FLUX_ROOT')) exit;
+$this->loginRequired();
+?>
+<h2><?php echo htmlspecialchars(Flux::message('FAHeader')) ?></h2>
+<p>Welcome to FluxAdmin, <?php echo $session->account->userid ?>. FluxAdmin is a tool to help you administrate your FluxCP installation from wherever you are.</p>
+<br />
+<h3>FluxCP Information</h3>
+<table width="100%">
+<tr><td width="33%">
+	<table class="horizontal-table" width="100%">
+	<tr><th colspan="2">Version Info</th></tr>
+	<tr><td>Latest Version</td><td>version</td></tr>
+	<tr><td>Current Version</td><td>version</td></tr>
+</table>
+	</td><td width="33%">
+	<table class="horizontal-table" width="100%">
+		<tr>
+			<th>Latest Commit</th>
+		</tr>
+		<?php echo $fdisplaycommit ?>
+	</table>
+</div>
+</td><td width="33%">
+	<table class="vertical-table" width="100%">
+		<tr>
+			<th>Pull Requests</th>
+		</tr>
+		<?php echo $fpulldisplay ?>
+	</table>
+</td></tr></table>
+<br />
+
+<h3>rAthena Information</h3>
+<table width="100%">
+<tr><td width="33%">
+	<table class="horizontal-table" width="100%">
+	<tr><th colspan="2">Version Info</th></tr>
+	<tr><td>Latest Version</td><td>version</td></tr>
+	<tr><td>Current Version</td><td>version</td></tr>
+</table>
+	</td><td width="33%">
+	<table class="horizontal-table" width="100%">
+		<tr>
+			<th>Latest Commit</th>
+		</tr>
+		<?php echo $rdisplaycommit ?>
+	</table>
+</div>
+</td><td width="33%">
+	<table class="vertical-table" width="100%">
+		<tr>
+			<th>Pull Requests</th>
+		</tr>
+		<?php echo $rpulldisplay ?>
+	</table>
+</td></tr></table>

e1c5c8c9b23260bfd3e8622d4fc68e0ef87a6e76
Mike Langford <mike.langford@live.co.uk>
2013-12-06 14:44:55 +0000

    News theme page now displays Facebook feed for pages

--- themes/default/news/index.php
+++ themes/default/news/index.php
@@ -60,9 +60,26 @@ $this->loginRequired();
 
 
 <?php elseif($newstype == '4'): ?>
+	<?php if(isset($xml) && isset($xml->channel)): ?>
+	<div class="newsDiv">
+		<?php foreach($xml->channel->item as $rssItem): ?>
+			<?php $i++; if($i <= $newslimit): ?>
+				<h2><?php echo $rssItem->title ?></h2>
+				<div class="newsCont">
+					<span class="newsDate"><small>Posted on <?php echo date(Flux::config('DateFormat'),strtotime($rssItem->pubDate))?></small></span>
+					<p><?php echo $rssItem->description ?></p>
+					<a class="news_link" href="<?php echo $rssItem->link ?>"><small><?php echo htmlspecialchars(Flux::message('XCMSNewsLink')) ?></small></a>
+					<div class="clear"></div>
+				</div>
+			<?php endif ?>
+		<?php endforeach; ?> 
+	</div>
+	<?php else: ?>
 		<p>
 			<?php echo htmlspecialchars(Flux::message('XCMSNewsFBNotFound')) ?><br/><br/>
 		</p>
+	<?php endif ?>
+
 
 
 

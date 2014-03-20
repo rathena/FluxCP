a577f1da1e6637457648559ce31e23a2ea3c94ed
anubisK <cagonza6@gmail.com>
2014-03-20 11:10:28 +0100

    Update view.php
    Added missing "_re", same than before. Also, there are some missing renewal chekings

--- modules/monster/view.php
+++ modules/monster/view.php
@@ -8,17 +8,19 @@ require_once 'Flux/TemporaryTable.php';
 
 // Monsters table.
 $mobDB      = "{$server->charMapDatabase}.monsters";
+//here needs the same check if the server is renewal or not, I'm just lazy to do it by myself
 $fromTables = array("{$server->charMapDatabase}.mob_db", "{$server->charMapDatabase}.mob_db2");
 $tempMobs   = new Flux_TemporaryTable($server->connection, $mobDB, $fromTables);
 
 // Monster Skills table.
 $skillDB    = "{$server->charMapDatabase}.mobskills";
+//here needs the same check if the server is renewal or not, I'm just lazy to do it by myself
 $fromTables = array("{$server->charMapDatabase}.mob_skill_db", "{$server->charMapDatabase}.mob_skill_db2");
 $tempSkills = new Flux_TemporaryTable($server->connection, $skillDB, $fromTables);
 
 // Items table.
 if($server->isRenewal) {
-	$fromTables = array("{$server->charMapDatabase}.item_db_re", "{$server->charMapDatabase}.item_db2");
+	$fromTables = array("{$server->charMapDatabase}.item_db_re", "{$server->charMapDatabase}.item_db2_re");
 } else {
 	$fromTables = array("{$server->charMapDatabase}.item_db", "{$server->charMapDatabase}.item_db2");
 }

cc67c9f21d62700df3bf3de845df406b7bb99aaf
anubisK <cagonza6@gmail.com>
2014-03-21 10:12:46 +0100

    Update view.php

--- modules/monster/view.php
+++ modules/monster/view.php
@@ -9,13 +9,22 @@ require_once 'Flux/TemporaryTable.php';
 // Monsters table.
 $mobDB      = "{$server->charMapDatabase}.monsters";
 //here needs the same check if the server is renewal or not, I'm just lazy to do it by myself
-$fromTables = array("{$server->charMapDatabase}.mob_db", "{$server->charMapDatabase}.mob_db2");
+if($server->isRenewal) {
+	$fromTables = array("{$server->charMapDatabase}.mob_db_re", "{$server->charMapDatabase}.mob_db2_re");
+} else {
+ 	$fromTables = array("{$server->charMapDatabase}.mob_db", "{$server->charMapDatabase}.mob_db2");
+}
 $tempMobs   = new Flux_TemporaryTable($server->connection, $mobDB, $fromTables);
 
 // Monster Skills table.
 $skillDB    = "{$server->charMapDatabase}.mobskills";
 //here needs the same check if the server is renewal or not, I'm just lazy to do it by myself
-$fromTables = array("{$server->charMapDatabase}.mob_skill_db", "{$server->charMapDatabase}.mob_skill_db2");
+if($server->isRenewal) {
+	$fromTables = array("{$server->charMapDatabase}.mob_skill_db_re", "{$server->charMapDatabase}.mob_skill_db2_re");
+} else {
+ 	$fromTables = array("{$server->charMapDatabase}.mob_skill_db", "{$server->charMapDatabase}.mob_skill_db2");
+}
+
 $tempSkills = new Flux_TemporaryTable($server->connection, $skillDB, $fromTables);
 
 // Items table.

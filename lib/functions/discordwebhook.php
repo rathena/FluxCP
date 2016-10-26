7d947ac307bd68c5017f628dae5f86fd09dd9082
Akkarinage <akkarin@rathena.org>
2016-10-26 17:18:30 +0100

    Create discordwebhook.php

---
+++ lib/functions/discordwebhook.php
@@ -0,0 +1,12 @@
+/**
+	Discord Webhook
+**/
+
+function sendtodiscord($url, $message) {
+    $data = array("content" => $message);
+    $curl = curl_init($url);
+    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    return curl_exec($curl);
+}

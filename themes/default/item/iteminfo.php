3dd0eaefd465e585f074693cefd5f87ef8e07cdd
Akkarinage <akkarin@rathena.org>
2017-02-17 03:34:05 +0000

    Adding php vars to itemInfo page
    Signed-off-by: Akkarinage <akkarin@rathena.org>

--- themes/default/item/iteminfo.php
+++ themes/default/item/iteminfo.php
@@ -5,6 +5,21 @@
     <p class="green"><?php echo htmlspecialchars($successMessage) ?></p>
 <?php endif ?>
 
+<h3>PHP Configuration</h3>
+<p>These values must be larger than the size of your itemInfo file.</p>
+<table class="vertical-table">
+	<tr>
+		<th>PHP Configs</th><td>Value</td>
+	</tr>
+	<tr>
+		<th>post_max_size</th><td><?php echo ini_get('post_max_size') ?></td>
+	</tr>
+	<tr>
+		<th>upload_max_filesize</th><td><?php echo ini_get('upload_max_filesize') ?></td>
+	</tr>
+</table>
+<p>ShowItemDesc is <?php if(Flux::config('ShowItemDesc')):?>enabled<?php else: ?>disabled<?php endif ?> in your configuration file.</p>
+
 <h3>Upload itemInfo.lua</h3>
 <form class="forms" method="post" enctype="multipart/form-data">
     <input type="file" name="iteminfo"><br>

70bf9948b7f0063529d28ddea9264e12442e6dcf
hazimjauhari90 <hazimjauhari@gmail.com>
2017-07-21 05:10:25 +0800

    Follow up to b9e4496
    Correct the symbol

--- data/schemas/logindb/cp_cmspages.20131120161901.sql
+++ data/schemas/logindb/cp_cmspages.20131120161901.sql
@@ -8,4 +8,5 @@ CREATE TABLE IF NOT EXISTS `cp_cmspages` (
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 INSERT INTO `cp_cmspages` (`id`, `path`, `title`, `body`, `modified`) VALUES
-(1, 'rules', 'Rules', 'This is a rules page.', '2013-11-20 00:00:00');
+(1, 'rules', 'Rules', 'This is a rules page.', '2013-11-20 00:00:00'),
+(2, 'downloads', 'Downloads', 'This is a download page.', '2017-07-20 00:00:00');

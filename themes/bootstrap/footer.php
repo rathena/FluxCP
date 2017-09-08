af3ab7ad5631085a25609a407e4a3353689c83a7
Akkarin <akkarin@rathena.org>
2017-09-08 15:38:28 +0100

    Removed Bootstrap CSS and JS files These are provided via a cdn, so to keep managable code size down, these have been removed from the repo This closes #147
    Signed-off-by: Akkarin <akkarin@rathena.org>

--- themes/bootstrap/footer.php
+++ themes/bootstrap/footer.php
@@ -132,6 +132,6 @@
 		<script type="text/javascript" src="<?php echo $this->themePath('js/flux.datefields.js') ?>"></script>
 		<script type="text/javascript" src="<?php echo $this->themePath('js/flux.unitip.js') ?>"></script>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="<?php echo $this->themePath('js/bootstrap.min.js') ?>"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>
--- themes/bootstrap/header.php
+++ themes/bootstrap/header.php
@@ -21,7 +21,7 @@
 		<!--[if IE]>
 		<link rel="stylesheet" href="<?php echo $this->themePath('css/flux/ie.css') ?>" type="text/css" media="screen" title="" charset="utf-8" />
 		<![endif]-->	
-		<link href="<?php echo $this->themePath('css/bootstrap.min.css') ?>" rel="stylesheet">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<link href="<?php echo $this->themePath('css/sticky-footer-navbar.css') ?>" rel="stylesheet">
 
 		<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->

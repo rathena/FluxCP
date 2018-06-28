71efe13f36c249fb8d9d6ab896376da5151970e1
Cydh Ramdh <cydh@pservero.com>
2018-06-28 22:08:56 +0700

    Corrected Inter log database lookup * Fixes #197

--- modules/logdata/inter.php
+++ modules/logdata/inter.php
@@ -3,7 +3,7 @@ if (!defined('FLUX_ROOT')) exit;
 
 $title = Flux::message('InterLogTitle');
 
-$sql = "SELECT COUNT(time) AS total FROM {$server->logsDatabase}.interlog";
+$sql = "SELECT COUNT(time) AS total FROM {$server->charMapDatabase}.interlog";
 $sth = $server->connection->getStatementForLogs($sql);
 $sth->execute();
 
@@ -11,7 +11,7 @@ $paginator = $this->getPaginator($sth->fetch()->total);
 $paginator->setSortableColumns(array('time' => 'log'));
 
 $col = 'time, log';
-$sql = $paginator->getSQL("SELECT $col FROM {$server->logsDatabase}.interlog");
+$sql = $paginator->getSQL("SELECT $col FROM {$server->charMapDatabase}.interlog");
 $sth = $server->connection->getStatementForLogs($sql);
 $sth->execute();
 

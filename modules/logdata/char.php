6900da9569d824a39fd8d21f2ead8ad76d83bb45
Cydh Ramdh <cydh@users.noreply.github.com>
2018-06-29 09:31:10 +0700

    Corrected Inter log database lookup (#198)
    * Corrected Inter log database lookup
    * Changed `getStatementForLogs` to `getStatement`
    * Fixes #197

--- modules/logdata/char.php
+++ modules/logdata/char.php
@@ -4,7 +4,7 @@ if (!defined('FLUX_ROOT')) exit;
 $title = Flux::message('CharLogTitle');
 
 $sql = "SELECT COUNT(time) AS total FROM {$server->charMapDatabase}.charlog";
-$sth = $server->connection->getStatementForLogs($sql);
+$sth = $server->connection->getStatement($sql);
 $sth->execute();
 
 $paginator = $this->getPaginator($sth->fetch()->total);
@@ -12,7 +12,7 @@ $paginator->setSortableColumns(array('time' => 'char_msg', 'account_id', 'char_n
 
 $col = 'time, char_msg, account_id, char_num, name';
 $sql = $paginator->getSQL("SELECT $col FROM {$server->charMapDatabase}.charlog ORDER BY `time` DESC");
-$sth = $server->connection->getStatementForLogs($sql);
+$sth = $server->connection->getStatement($sql);
 $sth->execute();
 
 $chars1 = $sth->fetchAll();

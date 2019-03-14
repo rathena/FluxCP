49cd6769df3d21ed006c86214643e9b760651db5
Akkarinage <akkarin@rathena.org>
2019-03-14 20:25:39 +0000

    Added explicit define for favicon (#227)

--- themes/installer/header.php
+++ themes/installer/header.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/x-icon" href="./favicon.ico" />
 		<title>FluxCP: Install &amp; Update</title>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 		<!--[if lt IE 9]>

ca133751a29485d3b26e9bfc9c9aca7991300054
danieldpl <56430929+danieldpl@users.noreply.github.com>
2019-10-11 05:12:47 -0300

    correct auth validation
    When md5 is active, the " trim($ password) == '' " condition is no longer valid, because even if the password is blank a 32 character code is generated.

--- lib/Flux/LoginServer.php
+++ lib/Flux/LoginServer.php
@@ -66,14 +66,15 @@ class Flux_LoginServer extends Flux_BaseServer {
 	 */
 	public function isAuth($username, $password)
 	{
-		if ($this->config->get('UseMD5')) {
-			$password = Flux::hashPassword($password);
-		}
 		
 		if (trim($username) == '' || trim($password) == '') {
 			return false;
 		}
-		
+
+     	if ($this->config->get('UseMD5')) {
+			$password = Flux::hashPassword($password);
+		}
+        
 		$sql  = "SELECT userid FROM {$this->loginDatabase}.login WHERE sex != 'S' AND group_id >= 0 ";
 		if ($this->config->getNoCase()) {
 			$sql .= 'AND LOWER(userid) = LOWER(?) ';

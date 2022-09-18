17bfe47d20f5fe8a343e5343c8882f76940f5817
Akkarinage <akkarin@rathena.org>
2022-09-18 14:19:54 +0100

    Updated methods for intemInfo parsing

--- modules/item/iteminfo.php
+++ modules/item/iteminfo.php
@@ -14,43 +14,89 @@ if($files->get('iteminfo')) {
         if($fp){ $array = explode("\n", fread($fp, filesize($itemInfo))); }
         fclose($fp);
         $ca = count($array);
-        $check = false;
+        $check = false; // True: execute query
+        $checkingdesc = false;
+        $desccomplete = false;
+        $last_setid = 0;
+        $desc = '';
+        $setid = 0;
+        $first = true;
+
         for($i=0; $i < $ca; $i++){
-            switch($array[$i]){
-                case (preg_match('/\s{2,}unident/', $array[$i]) ? true : false):
-                    break;
-                case (preg_match('/\s([\[])([0-9]{1,})([\]]) = {/', $array[$i]) ? true : false):
-                    $setid = preg_replace('/\s([\[])([0-9]{1,})([\]]) = {/', '$2', $array[$i]);
-                    break;
-                case (preg_match('/\sidentifiedDescriptionName = {$/', $array[$i]) ? true : false):
-                    $sql="REPLACE INTO {$server->charMapDatabase}.$itemDescTable (`itemid`, `itemdesc`) VALUES ('$setid','";
+            // Item ID
+            if (preg_match('/\[(\d+)\]/', $array[$i], $matches)) {
+                if ($first) {
+                    $setid = $matches[1];
+                    $first = false;
+                    continue;
+                }
+                if ($setid != $matches[1] && $checkingdesc) {
+                    $itemid = $setid;
+                    $desccomplete = true;
                     $check = true;
-                    break;
-                case 'identifiedDescriptionName = {},':
-                    if($check == true){
-                        $sql.="');";
-                        $sth = $server->connection->getStatement($sql);
-                        $sth->execute();
-                        $check = false;
+                }
+                $setid = $matches[1];
+            }
+
+            // Description Inline type
+            // identifiedDescriptionName = { "desc1", "desc2" },
+            if (preg_match('/^identifiedDescriptionName[ ]=[ ]\{(.*)\},/', $array[$i], $matches)) {
+                $tmp = trim($matches[1]);
+                $tmp = substr($tmp,0,strpos($tmp,"},"));
+                $str = preg_split('/(",|"$)/', $tmp);
+                foreach ($str as $x => $de) {
+                    $de = trim($de);
+                    $p = strtok($de,'"'); // Remove first quote
+                    $desc .= $p."<br />";
+                }
+                $check = true;
+                $desccomplete = true;
+            }
+
+            // Description Multiline type
+            // identifiedDescriptionName = {
+            //     "desc1",
+            //     "desc2"
+            // },
+            if (!$desccomplete && preg_match('/([ \s]+|^)identifiedDescriptionName[ ]=[ ]\{[\r\n]*/', $array[$i])) {
+                $checkingdesc = true;
+            }
+            if ($checkingdesc && preg_match('/"(.*)(",{0,1})[\r\n]*/', $array[$i], $matches)) {
+                $tmp = trim($matches[1]);
+                $desc .= $tmp;
+                if ($matches[2] == '",')
+                    $desc .= "<br />";
+            }
+
+            if ($check) {
+                $newdesc = '';
+                $hasColor = false;
+                $p = strtok($desc, "^");
+                while ($p) {
+                    if (preg_match('/([\dA-Fa-f]{6})/', $p, $matches)) {
+                        if ($hasColor)
+                            $newdesc .= "</font>";
+                        if ($matches[1] != '000000') {
+                            $newdesc .= "<font color='#".$matches[1]."'>";
+                            $hasColor = true;
+                        }
+                        else
+                            $hasColor = false;
+                        $newdesc .= substr($p,6,strlen($p));
                     }
-                    break;
-                case (preg_match('/(.*?)(\")(.*?)(\^)([0-9a-fA-F]{6})(.*?)(\^0{6})(.*?)\"(,?)/', $array[$i]) ? true : false):
-                    $array[$i] = preg_replace('/(.*?)(\")(.*?)(\^)([0-9a-fA-F]{6})(.*?)(\^0{6})(.*?)\"(,?)/', '$3<font color="#$5">$6</font>$8<br />', $array[$i]);
-                case (preg_match('/\s{2,}(\")(.*?)\"(,?)/', $array[$i]) ? true : false):
-                    if($check == true){
-                        $sqlp = preg_replace('/\s{2,}(\")(.*?)\"(,?)/', '$2<br />', $array[$i]);
-                        $sql.= addslashes($sqlp);
-                    }
-                    break;
-                case (preg_match('/\s{2,}},/', $array[$i]) ? true : false):
-                    if($check == true){
-                        $sql.="');";
-                        $sth = $server->connection->getStatement($sql);
-                        $sth->execute();
-                        $check = false;
-                    }
-                default:
-                    break;
+                    else
+                        $newdesc .= $p;
+                    $p = strtok("^");
+                }
+                if ($hasColor)
+                    $newdesc .= "</font>";
+                $sql = "REPLACE INTO {$server->charMapDatabase}.$itemDescTable (`itemid`, `itemdesc`) VALUES ('".($checkingdesc ? $itemid : $setid)."','".addslashes($newdesc)."')";
+                $sth = $server->connection->getStatement($sql);
+                $sth->execute();
+                $desc = '';
+                $check = false;
+                $checkingdesc = false;
+                $desccomplete = false;
             }
         }
         $fileLoad->delete();

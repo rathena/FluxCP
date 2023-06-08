d07f1deb022d920339bc52f90a5d7afb80709aa8
Akkarinage <mike.langford@lustinteractive.co.uk>
2023-06-08 15:43:37 +0100

    Whitespace cleansing in Bootstrap theme

--- themes/bootstrap/footer.php
+++ themes/bootstrap/footer.php
@@ -8,14 +8,13 @@
 				Powered by <a href="https://github.com/rathena/FluxCP" target="_blank">FluxCP</a>
 				<?php endif ?>
 				<?php if (Flux::config('ShowRenderDetails')): ?>
-				
+
 					Page generated in <strong><?php echo round(microtime(true) - __START__, 5) ?></strong> second(s).
 					Number of queries executed: <strong><?php echo (int)Flux::$numberOfQueries ?></strong>.
 					<?php if (Flux::config('GzipCompressOutput')): ?>Gzip Compression: <strong>Enabled</strong>.<?php endif ?>
-				
+
 				<?php endif ?>
 				<?php if (count(Flux::$appConfig->get('ThemeName', false)) > 1): ?>
-				
 					<span>Theme:
 						<select name="preferred_theme" onchange="updatePreferredTheme(this)">
 						<?php foreach (Flux::$appConfig->get('ThemeName', false) as $themeName): ?>
@@ -23,83 +22,80 @@
 						<?php endforeach ?>
 						</select>
 					</span>
-					<?php endif ?>
+				<?php endif ?>
 
-                <span>Language:
-						<select name="preferred_language" onchange="updatePreferredLanguage(this)">
-						<?php foreach (Flux::getAvailableLanguages() as $lang_key => $lang): ?>
-                            <option value="<?php echo htmlspecialchars($lang_key) ?>"<?php if (!empty($_COOKIE['language']) && $_COOKIE['language'] == $lang_key) echo ' selected="selected"' ?>><?php echo htmlspecialchars($lang) ?></option>
-                        <?php endforeach ?>
-						</select>
-					</span>
-
-            </p>
-            <form action="<?php echo $this->urlWithQs ?>" method="post" name="preferred_theme_form" style="display: none">
-                <input type="hidden" name="preferred_theme" value="" />
-            </form>
+				<span>Language:
+					<select name="preferred_language" onchange="updatePreferredLanguage(this)">
+					<?php foreach (Flux::getAvailableLanguages() as $lang_key => $lang): ?>
+						<option value="<?php echo htmlspecialchars($lang_key) ?>"<?php if (!empty($_COOKIE['language']) && $_COOKIE['language'] == $lang_key) echo ' selected="selected"' ?>><?php echo htmlspecialchars($lang) ?></option>
+					<?php endforeach ?>
+					</select>
+				</span>
+			</p>
+			<form action="<?php echo $this->urlWithQs ?>" method="post" name="preferred_theme_form" style="display: none">
+				<input type="hidden" name="preferred_theme" value="" />
+			</form>
 		</div>
-    </div>
+	</div>
 
 
 		<!--[if lt IE 9]>
 		<script src="<?php echo $this->themePath('js/ie9.js') ?>" type="text/javascript"></script>
-		<script type="text/javascript" src="<?php echo $this->themePath('js/flux.unitpngfix.js') ?>"></script>
 		<![endif]-->
-		
+
 		<script type="text/javascript">
 			function updatePreferredServer(sel){
 				var preferred = sel.options[sel.selectedIndex].value;
 				document.preferred_server_form.preferred_server.value = preferred;
 				document.preferred_server_form.submit();
 			}
-			
+
 			function updatePreferredTheme(sel){
 				var preferred = sel.options[sel.selectedIndex].value;
 				document.preferred_theme_form.preferred_theme.value = preferred;
 				document.preferred_theme_form.submit();
 			}
 
-            function updatePreferredLanguage(sel){
-                var preferred = sel.options[sel.selectedIndex].value;
-                setCookie('language', preferred);
-                reload();
-            }
+			function updatePreferredLanguage(sel){
+				var preferred = sel.options[sel.selectedIndex].value;
+				setCookie('language', preferred);
+				reload();
+			}
 
 			// Preload spinner image.
 			var spinner = new Image();
 			spinner.src = '<?php echo $this->themePath('img/spinner.gif') ?>';
-			
+
 			function refreshSecurityCode(imgSelector){
 				$(imgSelector).attr('src', spinner.src);
-				
+
 				// Load image, spinner will be active until loading is complete.
 				var clean = <?php echo Flux::config('UseCleanUrls') ? 'true' : 'false' ?>;
 				var image = new Image();
 				image.src = "<?php echo $this->url('captcha') ?>"+(clean ? '?nocache=' : '&nocache=')+Math.random();
-				
+
 				$(imgSelector).attr('src', image.src);
 			}
-			function toggleSearchForm()
-			{
+			function toggleSearchForm(){
 				//$('.search-form').toggle();
 				$('.search-form').slideToggle('fast');
 			}
 
-            function setCookie(key, value) {
-                var expires = new Date();
-                expires.setTime(expires.getTime() + expires.getTime()); // never expires
-                document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
-            }
+			function setCookie(key, value){
+				var expires = new Date();
+				expires.setTime(expires.getTime() + expires.getTime()); // never expires
+				document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+			}
 		</script>
-		
+
 		<?php if (Flux::config('EnableReCaptcha') && Flux::config('ReCaptchaTheme')): ?>
 			<script type="text/javascript">
-				 var RecaptchaOptions = {
+				var RecaptchaOptions = {
 					theme : '<?php echo Flux::config('ReCaptchaTheme') ?>'
-				 };
+				};
 			</script>
 		<?php endif ?>
-		
+
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script type="text/javascript">
 			$(document).ready(function(){
@@ -142,18 +138,11 @@
 					else
 						$('.money-input').val(moneyValue.toFixed(2));
 				}).keyup();
-				
-				// In: js/flux.datefields.js
-				processDateFields();
 			});
-			
-			function reload(){
-				window.location.href = '<?php echo $this->url ?>';
-			}
+
+			function reload(){ window.location.href = '<?php echo $this->url ?>'; }
 		</script>
 
-		<script type="text/javascript" src="<?php echo $this->themePath('js/flux.datefields.js') ?>"></script>
-		<script type="text/javascript" src="<?php echo $this->themePath('js/flux.unitip.js') ?>"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</body>
 </html>

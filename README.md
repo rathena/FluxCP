d21abe00ff3f1473780e9c107f2b1099d41a6344
Akkarinage <mike.langford@lustinteractive.co.uk>
2023-06-08 15:11:12 +0100

    Deepscan Error Mitigation (#355)

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 FluxCP
 ======
-[![Total alerts](https://img.shields.io/lgtm/alerts/g/rathena/FluxCP.svg?logo=lgtm&logoWidth=18&color=orange)](https://lgtm.com/projects/g/rathena/FluxCP/alerts/)
+[![DeepSource](https://app.deepsource.com/gh/rathena/FluxCP.svg/?label=active+issues&show_trend=true&token=nhkIfid6qRIZxl2INWaaV4Qb)](https://app.deepsource.com/gh/rathena/FluxCP/?ref=repository-badge)
 [![Open Issues](https://img.shields.io/github/issues/rathena/FluxCP.svg?logo=github&logoWidth=18&color=yellow)](https://lgtm.com/projects/g/rathena/FluxCP/alerts/)
 [![Open PRs](https://img.shields.io/github/issues-pr/rathena/FluxCP.svg?logo=github&logoWidth=18&color=blue)](https://lgtm.com/projects/g/rathena/FluxCP/alerts/)
 [![Codacy Badge](https://app.codacy.com/project/badge/Grade/4d1c0a43c0864764b3d3dc5ed2d93192)](https://www.codacy.com/gh/rathena/FluxCP/dashboard?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=rathena/FluxCP&amp;utm_campaign=Badge_Grade)
--- lib/functions/getReposVersion.php
+++ lib/functions/getReposVersion.php
@@ -7,13 +7,11 @@
 function getReposVersion()
 {
 	$gitDir = FLUX_ROOT.'/.git';
-	$svnDir = FLUX_ROOT.'/.svn';
-	
-	if (is_dir($gitDir)) {
+
+	if(is_dir($gitDir)) {
 		return git_hash();
-	}
-	else if (is_dir($svnDir)) {
-		return svn_version();
+	} else {
+		return null;
 	}
 }
 
@@ -23,63 +21,17 @@ function getReposVersion()
  * @param string file name.
  * @return int GIT hash
  */
-function git_hash()
-{
+function git_hash() {
 	$file = FLUX_ROOT.'/.git/refs/heads/master';
-	
 	if (file_exists($file) && is_readable($file)) {
 		$lines = implode('', file($file, FILE_IGNORE_NEW_LINES|FILE_SKIP_EMPTY_LINES));
-		
 		if(isset($lines)) {
 			return trim(substr($lines, 0, 10));
 		}
 		return null;
+	} else {
+		return null;
 	}
-}
-
-/**
- * Get the SVN revision of a directory.
- *
- * @param string `entries' file name.
- * @return int Revision number
- */
-function svn_version()
-{
-	$rev = null;
-	
-	// Subversion 1.6 and lower
-	$file = FLUX_ROOT.'/.svn/entries';
-	if (file_exists($file) && is_readable($file)) {
-		$fp  = fopen($file, 'r');
-		$arr = explode("\n", fread($fp, 256));
-
-		if (isset($arr[3]) && ctype_digit($found = trim($arr[3]))) {
-			$rev = $found;
-		}
-		fclose($fp);
-	}
-	
-	//Subversion 1.7 and up
-	if(!isset($rev)) {
-		$file = FLUX_ROOT.'/.svn/wc.db';
-		$curr = 0;
-		
-		if (file_exists($file) && is_readable($file)) {
-			$fp  = fopen($file, 'r');
-			
-			while(($line = fread($fp, 64))) {
-				if(strstr($line,"!svn/ver/") && sscanf(strstr($line,"!svn/ver/"),"!svn/ver/%d/%*s", $curr) == 1) {
-					if($curr > $rev) {
-						$rev = $curr;
-					}
-				}
-			}
-			
-			fclose($fp);
-		}
-	}
-	
-	return $rev;	
 }
 
 ?>

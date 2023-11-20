87c7b7c75a3f67ce052b2c92f66f2f8c2c477f15
jowie <53240858+gamingmagic@users.noreply.github.com>
2023-11-20 14:35:38 +0800

    adding economy on castle
    adding economy on castle

--- modules/castle/index.php
+++ modules/castle/index.php
@@ -6,7 +6,7 @@ $title = 'Castles';
 $castleNames = Flux::config('CastleNames')->toArray();
 $ids = implode(',', array_fill(0, count($castleNames), '?'));
 
-$sql  = "SELECT castles.castle_id, castles.guild_id, guild.name AS guild_name, guild.emblem_id as emblem ";
+$sql  = "SELECT castles.castle_id, castles.guild_id, castles.economy, guild.name AS guild_name, guild.emblem_id as emblem ";
 $sql .= "FROM {$server->charMapDatabase}.guild_castle AS castles ";
 $sql .= "LEFT JOIN guild ON guild.guild_id = castles.guild_id ";
 $sql .= "WHERE castles.castle_id IN ($ids)";
